a613fcd8f328f1b7fdccf5504f7a23232055d6b7
MoazAbdullah1 <moazabdullah02@gmail.com>
2023-08-30 00:38:33 +0500

    2nd Project Quizz Maker

---
+++ Quizz Maker/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const createQuizBtn = document.getElementById("createQuizBtn");
+    const takeQuizBtn = document.getElementById("takeQuizBtn");
+
+    createQuizBtn.addEventListener("click", () => {
+        
+
+    });
+
+    takeQuizBtn.addEventListener("click", () => {
+        
+        
+    });
+});
