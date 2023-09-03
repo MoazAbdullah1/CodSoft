a5dcdb1a076a697dd719710d0a8370dd929a24f1
MoazAbdullah1 <moazabdullah02@gmail.com>
2023-09-03 13:54:49 +0500

    4th Project

--- Quizz Maker/script.js
+++ Quizz Maker/script.js
@@ -3,12 +3,37 @@ document.addEventListener("DOMContentLoaded", () => {
     const takeQuizBtn = document.getElementById("takeQuizBtn");
 
     createQuizBtn.addEventListener("click", () => {
-        
-
+        // Redirect to quiz creation page or open a modal
     });
 
     takeQuizBtn.addEventListener("click", () => {
         
-        
+        // Redirect to quiz listing page or open a modal
     });
 });
+
+
+
+function checkAnswer() {
+            
+    var selectedOption = document.querySelector('input[name="answer"]:checked');
+    
+    
+    if (selectedOption) {
+        var userAnswer = selectedOption.value;
+        
+        var correctAnswer = "C";
+
+        
+        if (userAnswer === correctAnswer) {
+            alert("Correct answer!");
+        } else {
+            alert("Incorrect answer. Try again.");
+        }
+    } else {
+        alert("Please select an answer.");
+    }
+}
+
+
+
