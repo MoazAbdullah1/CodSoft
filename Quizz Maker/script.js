46042fbd31cd3e85c60e0c03cd251b675c6d2656
MoazAbdullah1 <moazabdullah02@gmail.com>
2023-09-06 00:16:16 +0500

    Take Quizz Style and functionalities

--- Quizz Maker/script.js
+++ Quizz Maker/script.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const takeQuizBtn = document.getElementById("takeQuizBtn");
 
     createQuizBtn.addEventListener("click", () => {
-        // Redirect to quiz creation page or open a modal
+        
     });
 
     takeQuizBtn.addEventListener("click", () => {
         
-        // Redirect to quiz listing page or open a modal
+        
     });
 });
 
@@ -34,6 +34,90 @@ function checkAnswer() {
         alert("Please select an answer.");
     }
 }
+
+
+
+
+
+
+
+
+        var currentQuestion = 0;
+        var questions = [
+            {
+                question: "Which planet is known as the 'Red Planet'?",
+                options: ["Earth", "Venus", "Mars", "Jupiter"],
+                correctAnswer: "C"
+            },
+            {
+                question: "What is the capital of France?",
+                options: ["Berlin", "Madrid", "Paris", "Rome"],
+                correctAnswer: "C"
+            },
+            // Add more questions here
+        ];
+
+        function showQuestion() {
+            if (currentQuestion < questions.length) {
+                var questionObj = questions[currentQuestion];
+
+                var questionCard = document.getElementById("question-card");
+
+                questionCard.innerHTML = `
+                    <h1>Multiple Choice Question</h1>
+                    <p><strong>Question:</strong> ${questionObj.question}</p>
+                    <form>
+
+                        ${questionObj.options.map((option, index) => `
+                            <label>
+                                <input type="radio" name="answer" value="${String.fromCharCode(65 + index)}"> ${String.fromCharCode(65 + index)}) ${option}
+                            </label><br>
+                        `).join("")}
+                        <br>
+                        <button type="button" onclick="checkAnswer()">Submit Answer</button>
+                    </form>
+                `;
+
+            }
+            
+            else {
+                var questionCard = document.getElementById("question-card");
+                questionCard.innerHTML = "<h1>Quiz Complete</h1><p>Congratulations! You have completed the quiz.</p>";
+            }
+        }
+
+        
+
+        function checkAnswer() {
+            var selectedOption = document.querySelector('input[name="answer"]:checked');
+            
+            if (selectedOption) {
+                var userAnswer = selectedOption.value;
+                var correctAnswer = questions[currentQuestion].correctAnswer;
+
+                if (userAnswer === correctAnswer) {
+                    alert("Correct answer!");
+
+                    // Move to the next question
+                    currentQuestion++;
+                    showQuestion();
+                } else {
+                    alert("Incorrect answer. Try again.");
+                }
+            } else {
+                alert("Please select an answer.");
+            }
+        }
+
+        
+
+        window.onload = showQuestion;
+
+
+
+
+
+
 
 
 
